b7309935733ae901426271397e71ec50562a4179
oga-0927 <totti0927.oga@gmail.com>
2021-02-04 23:13:01 +0900

    configとerror

--- src/main.js
+++ src/main.js
@@ -6,7 +6,16 @@ Vue.config.productionTip = false
 
 axios.defaults.baseURL = "https://firestore.googleapis.com/v1/projects/vuejs-http-316b3/databases/(default)/documents";
 
-axios.interceptors.request.use();
+axios.interceptors.request.use(
+  // 機能の追加ができる
+  config => {
+    console.log('interceptors.request', config);
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  }
+);
 axios.interceptors.response.use();
 
 
